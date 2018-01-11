9050f1a7596fbf53e17d7be41f91c1ed4ae40016
Programista1 <marekjedrysiak1@gamil.com>
2018-01-11 14:58:28 +0100

    pierwszy commit

---
+++ MyStoreDomain/Concrete/SfcDb.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using MyStoreDomain.Entities;
+
+
+namespace MyStoreDomain.Concrete
+{
+    public class SfcDb : DbContext // conection to data base 
+    {
+        public DbSet<MyItem> myItems { get; set; }
+    }
+}
+//SfcDb
